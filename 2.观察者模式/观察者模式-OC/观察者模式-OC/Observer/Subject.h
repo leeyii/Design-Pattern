0eaaa1dede86f575769dae4cf8f00a2fee7e3506
leeyii <lijie_1@doweidu.com>
2019-03-24 23:36:11 +0800

    观察者的优劣

--- 2.观察者模式/观察者模式-OC/观察者模式-OC/Observer/Subject.h
+++ 2.观察者模式/观察者模式-OC/观察者模式-OC/Observer/Subject.h
@@ -16,7 +16,8 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)subscribe:(id <Observer>)subscriber;
 
 - (void)unsubscribe:(id <Observer>)subscriber;
-
+// 通知观察值观察对象发生变化
+// 使用id类型 只能通过观察者和被观察者直接约定进行通信
 - (void)notifySubscriber:(id)userinfo;
 
 @end
