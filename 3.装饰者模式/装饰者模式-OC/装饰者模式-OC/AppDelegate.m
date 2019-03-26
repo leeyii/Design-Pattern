d53f29127695990169c068f9d252456877ba237d
leeyii <lijie_666666@126.com>
2019-03-26 23:36:43 +0800

    test

--- 3.装饰者模式/装饰者模式-OC/装饰者模式-OC/AppDelegate.m
+++ 3.装饰者模式/装饰者模式-OC/装饰者模式-OC/AppDelegate.m
@@ -21,7 +21,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    NSLog(@"test");
+    NSLog(@"test1");
     id <Component> comp = [[ConcreteComponent alloc] init];
     comp = [[ConcreteDecoratorA alloc] initComponent:comp];
     comp = [[ConcreteDecoratorB alloc] initComponent:comp];
