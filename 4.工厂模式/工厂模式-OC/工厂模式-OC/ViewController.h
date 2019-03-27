bc21d45665433c299104804fb3c8e19dd6aea563
leeyii <lijie_666666@126.com>
2019-03-27 22:39:20 +0800

    init 工厂模式

---
+++ 4.工厂模式/工厂模式-OC/工厂模式-OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  工厂模式-OC
+//
+//  Created by leeyii on 2019/3/27.
+//  Copyright © 2019 leeyii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 4.工厂模式/工厂模式-OC/工厂模式-OC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  工厂模式-OC
+//
+//  Created by leeyii on 2019/3/27.
+//  Copyright © 2019 leeyii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
